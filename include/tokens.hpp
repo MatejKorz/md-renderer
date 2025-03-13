c56a870e3dec9251626eedc48f719f5edae2f1fe
MatejKorz <korz.matej@gmail.com>
2025-03-13 21:33:20 +0100

    feat: basic token map

--- include/tokens.hpp
+++ include/tokens.hpp
@@ -1,17 +1,26 @@
 #include <string>
+#include <map>
 
 enum class TokenType {
-    Heading,   // #, ##, ###
-    Bold,      // **bold**
-    Italic,    // *italic*
-    ListItem,  // - or * list item
-    CodeBlock, // ```code```
-    Text,      // Plain text
-    Link,      // [text](url)
-    EndOfFile  // EOF
+    Heading,
+    BoldItalic,
+    BlockQoute,
+    ListItem,
+    CodeBlock,
+    EscapeChar,
 };
 
 struct Token {
     TokenType type;
     std::string value;
+};
+
+const std::map<std::string, TokenType> tokenMap = {
+    {"#", TokenType::Heading},
+    {"*", TokenType::BoldItalic},
+    {"_", TokenType::BoldItalic},
+    {">", TokenType::BlockQoute},
+    {"-", TokenType::ListItem},
+    {"`", TokenType::CodeBlock},
+    {"\\", TokenType::EscapeChar},
 };
